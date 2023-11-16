661f466a61a8c01f6cc0c4812870ac0276f6bd74
Musievska <dilyana.musievska@gmail.com>
2023-11-16 10:51:05 +0200

    product migration running

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen  justify-center px-2 sm:px-4 md:px-8 bg-no-repeat bg-center bg-cover"
+         style={{ backgroundImage: "url('/page.png')" }}>
+      <Link href="/products">
+        <span className="text-4xl font-bold text-red-500 cursor-pointer">
+          Explore Space Goods
+        </span>
+      </Link>
+    </div>
+  );
+}
